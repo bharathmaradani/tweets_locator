1a78b9d88c9cd6ccd3bf18e1cf59875372632ba9
bharathmaradani <bharathmaradani99@gmail.com>
2017-01-28 00:19:23 +0530

    Adding secret token file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TweetsLocator::Application.config.secret_token = '04b622dde67b5da00c831853352523eb25e095e4c2eb10961873e2b25cb7ef647625c98eba7811543a79a71db3ed4944b59f4f503696ce369eb0e2a9fdaa2d2f'
